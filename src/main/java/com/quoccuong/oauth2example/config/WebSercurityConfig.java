9ba5d78460c2ba8c0d48aa87ddf26901213035db
quoccuong <tqcuong.ptit@gmail.com>
2019-01-27 16:39:57 +0700

    Add comment for the sql to create oauth_access_token and access_token tables.

--- src/main/java/com/quoccuong/oauth2example/config/WebSercurityConfig.java
+++ src/main/java/com/quoccuong/oauth2example/config/WebSercurityConfig.java
@@ -70,8 +70,29 @@ public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public TokenStore tokenStore() {
-//        return new InMemoryTokenStore();
+/*
+CREATE TABLE `oauth_access_token` (
+`authentication_id` varchar(255) NOT NULL,
+`token_id` varchar(255) NOT NULL,
+`token` blob NOT NULL,
+`user_name` varchar(255) NOT NULL,
+`client_id` varchar(255) NOT NULL,
+`authentication` blob NOT NULL,
+`refresh_token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `oauth_access_token`
+ADD PRIMARY KEY (`authentication_id`);
+
+
+CREATE TABLE `oauth_refresh_token` (
+`token_id` varchar(255) NOT NULL,
+`token` blob NOT NULL,
+`authentication` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+*/
         return new JdbcTokenStore(dataSource);
+//        return new InMemoryTokenStore();
     }
 
     @Bean
